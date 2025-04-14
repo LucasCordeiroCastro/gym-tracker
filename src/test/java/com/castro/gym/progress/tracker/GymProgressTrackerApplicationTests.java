92015d9bfd6c0399e4f64591949b6851d9bf479d
Lucas <lucascordeirocastro@gmail.com>
2025-04-13 21:32:17 -0300

    Project structure

---
+++ src/test/java/com/castro/gym/progress/tracker/GymProgressTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.castro.gym.progress.tracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GymProgressTrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
